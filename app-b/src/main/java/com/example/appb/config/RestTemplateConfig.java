3ab3cf1629a87db409a532acefa96d920162dd0b
Gabriel Jeronimo Lima Silva <gabriel.jeronimo.lima.silva@f1rst.com.br>
2025-09-01 14:24:08 -0300

    remove development bypass from the codebase

--- app-b/src/main/java/com/example/appb/config/RestTemplateConfig.java
+++ app-b/src/main/java/com/example/appb/config/RestTemplateConfig.java
@@ -17,41 +17,6 @@ public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        try {
-            TrustManager[] trustAllCerts = new TrustManager[]{
-                    new X509TrustManager() {
-                        public X509Certificate[] getAcceptedIssuers() {
-                            return null;
-                        }
 
-                        public void checkClientTrusted(X509Certificate[] certs, String authType) {
-                        }
-
-                        public void checkServerTrusted(X509Certificate[] certs, String authType) {
-                        }
-                    }
-            };
-
-            SSLContext sslContext = SSLContext.getInstance("SSL");
-            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
-            HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
-
-            HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
-
-            SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory() {
-                @Override
-                protected void prepareConnection(HttpURLConnection connection, String httpMethod) throws java.io.IOException {
-                    super.prepareConnection(connection, httpMethod);
-                    if (connection instanceof HttpsURLConnection) {
-                        ((HttpsURLConnection) connection).setSSLSocketFactory(sslContext.getSocketFactory());
-                        ((HttpsURLConnection) connection).setHostnameVerifier((hostname, session) -> true);
-                    }
-                }
-            };
-
-            return new RestTemplate(requestFactory);
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to create RestTemplate with SSL bypass", e);
-        }
     }
 }
